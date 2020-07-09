015cb1632dca624284add986b22d4659c21ff977
sdwfhjq123 <sdwfhjq123@qq.com>
2020-07-09 17:57:32 +0800

    '设计架构'

--- app/src/main/java/com/yinhao/wanandroid/ui/home/HomeActivity.kt
+++ app/src/main/java/com/yinhao/wanandroid/ui/home/HomeActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.yinhao.commonmodule.base.base.BaseActivity
 import com.yinhao.wanandroid.R
 import com.yinhao.wanandroid.databinding.ActivityHomeBinding
 import com.yinhao.wanandroid.widget.ToolbarManager
 
+//TODO 考虑是采用github的方式还是简书 https://www.jianshu.com/p/41d0e33bb674
 class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>(), ToolbarManager {
     override val toolbar: Toolbar by lazy { viewBinding!!.toolbar }
 
@@ -24,13 +26,22 @@ class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>(), Toolbar
     }
 
     override fun initEvents() {
+        setSupportActionBar(toolbar)
+        enableHomeAsUp(1f) {
+//            viewBinding?.drawerLayout.star
+        }
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        toolbar.setupWithNavController(navController, appBarConfiguration)
-        viewBinding?.navView?.let { bnv ->
-            bnv.setupWithNavController(navController)
-            bnv.setOnNavigationItemSelectedListener {
+        val appBarConfiguration =
+            AppBarConfiguration(
+                setOf(R.id.nav_home, R.id.nav_knowledge, R.id.nav_nav, R.id.nav_project),
+                viewBinding?.drawerLayout
+            )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        viewBinding?.navView?.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemSelectedListener {
                 when (it.itemId) {
                     R.id.nav_home -> navController.navigate(R.id.homeFragment)
                     R.id.nav_knowledge -> navController.navigate(R.id.knowledgeFragment)
